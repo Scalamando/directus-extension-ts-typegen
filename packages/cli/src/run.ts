82afaa0edc747161a7f76899a58d887af2a7de40
Raimund Canzler <git@rai-canzler.de>
2025-07-11 01:05:55 +0200

    fix: handle sigint during inquirer prompts

--- packages/cli/src/run.ts
+++ packages/cli/src/run.ts
@@ -47,150 +47,160 @@ yargs(hideBin(process.argv))
     "generate the types",
     () => {},
     async (argv) => {
-      if (argv.verbose) console.info(`starting generation`);
+      try {
+        if (argv.verbose) console.info(`starting generation`);
 
-      let host = argv.directusHost ?? process.env.DIRECTUS_TS_TYPEGEN_HOST ?? "";
-      let email = argv.directusEmail ?? process.env.DIRECTUS_TS_TYPEGEN_EMAIL ?? "";
-      let password = argv.directusPassword ?? process.env.DIRECTUS_TS_TYPEGEN_PASSWORD ?? "";
-      let token = argv.directusToken ?? process.env.DIRECTUS_TS_TYPEGEN_TOKEN ?? "";
-      let output = argv.output ?? process.env.DIRECTUS_TS_TYPEGEN_OUTPUT ?? "";
+        let host = argv.directusHost ?? process.env.DIRECTUS_TS_TYPEGEN_HOST ?? "";
+        let email = argv.directusEmail ?? process.env.DIRECTUS_TS_TYPEGEN_EMAIL ?? "";
+        let password = argv.directusPassword ?? process.env.DIRECTUS_TS_TYPEGEN_PASSWORD ?? "";
+        let token = argv.directusToken ?? process.env.DIRECTUS_TS_TYPEGEN_TOKEN ?? "";
+        let output = argv.output ?? process.env.DIRECTUS_TS_TYPEGEN_OUTPUT ?? "";
 
-      if (host == "") {
-        host = await input({
-          message: "Please enter the url of your directus instance:",
-          required: true,
-          validate: async (value) => {
-            let url: URL;
-            try {
-              url = new URL(value);
-            } catch (_err) {
-              return "Invalid URL. Please check for typos.";
-            }
-
-            try {
-              const ping = await fetch(url);
-              const isReachable = ping.status >= 200 && ping.status < 300;
-              if (isReachable) return true;
-
-              return "This url cannot be reached.";
-            } catch (_err) {
-              return "This url cannot be reached.";
-            }
-          },
-        });
-      }
-
-      let tokenOrPasswordAuth: null | "password" | "token" =
-        password != "" ? "password" : token != "" ? "token" : null;
-      if (tokenOrPasswordAuth == null) {
-        tokenOrPasswordAuth = await select({
-          message: "How do you want to authenticate?",
-          choices: [
-            { name: "Email and Password", value: "password", separator: " " },
-            { name: "Static Access Token", value: "token", separator: " " },
-          ],
-        });
-      }
-
-      if (tokenOrPasswordAuth == "password") {
-        if (email == "") {
-          email = await input({
-            message: "Please enter your email address:",
+        if (host == "") {
+          host = await input({
+            message: "Please enter the url of your directus instance:",
             required: true,
-            validate: (value) => value.includes("@"),
+            validate: async (value) => {
+              let url: URL;
+              try {
+                url = new URL(value);
+              } catch (_err) {
+                return "Invalid URL. Please check for typos.";
+              }
+
+              try {
+                const ping = await fetch(url);
+                const isReachable = ping.status >= 200 && ping.status < 300;
+                if (isReachable) return true;
+
+                return "This url cannot be reached.";
+              } catch (_err) {
+                return "This url cannot be reached.";
+              }
+            },
           });
         }
-        if (password == "") {
-          password = await passwordInput({
-            message: "Please enter your password:",
-            mask: "*",
+
+        let tokenOrPasswordAuth: null | "password" | "token" =
+          password != "" ? "password" : token != "" ? "token" : null;
+        if (tokenOrPasswordAuth == null) {
+          tokenOrPasswordAuth = await select({
+            message: "How do you want to authenticate?",
+            choices: [
+              { name: "Email and Password", value: "password", separator: " " },
+              { name: "Static Access Token", value: "token", separator: " " },
+            ],
           });
         }
-      } else if (token == "") {
-        token = await passwordInput({
-          message: "Please enter your static access token:",
-        });
-      }
 
-      let bearerToken: string;
-      if (tokenOrPasswordAuth == "password") {
-        const responseBody = await fetch(path.join(host, "/auth/login"), {
-          method: "POST",
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then((res) => res.json() as Promise<{ data: { access_token: string } }>);
-        bearerToken = responseBody.data.access_token;
-      } else {
-        bearerToken = token;
-      }
-
-      const headers = {
-        Authorization: "Bearer " + bearerToken,
-      };
-
-      const collections = await fetch(path.join(host, "/collections"), { headers })
-        .then((res) => res.json() as Promise<{ data: DirectusCollection[] } | { errors: string[] }>)
-        .then((res) => {
-          if ("errors" in res) {
-            throw new Error(
-              `One or more errors occured when fetching the collections: \n${res.errors.join("\n")}`
-            );
+        if (tokenOrPasswordAuth == "password") {
+          if (email == "") {
+            email = await input({
+              message: "Please enter your email address:",
+              required: true,
+              validate: (value) => value.includes("@"),
+            });
           }
-          return res.data;
-        });
-
-      const fields = await fetch(path.join(host, "/fields"), { headers })
-        .then((res) => res.json() as Promise<{ data: DirectusField[] } | { errors: string[] }>)
-        .then((res) => {
-          if ("errors" in res) {
-            throw new Error(
-              `One or more errors occured when fetching the fields: \n${res.errors.join("\n")}`
-            );
+          if (password == "") {
+            password = await passwordInput({
+              message: "Please enter your password:",
+              mask: "*",
+            });
           }
-          return res.data;
-        });
-
-      const relations = await fetch(path.join(host, "/relations"), { headers })
-        .then((res) => res.json() as Promise<{ data: DirectusRelation[] } | { errors: string[] }>)
-        .then((res) => {
-          if ("errors" in res) {
-            throw new Error(
-              `One or more errors occured when fetching the relations: \n${res.errors.join("\n")}`
-            );
-          }
-          return res.data;
-        });
-
-      const types = generateTypes(
-        { collections, fields, relations },
-        {
-          typePrefix: "",
-          requiredNotNullable: false,
+        } else if (token == "") {
+          token = await passwordInput({
+            message: "Please enter your static access token:",
+          });
         }
-      );
 
-      if (output == "") {
-        process.stdout.write(types + "\n");
-        return;
+        let bearerToken: string;
+        if (tokenOrPasswordAuth == "password") {
+          const responseBody = await fetch(path.join(host, "/auth/login"), {
+            method: "POST",
+            body: JSON.stringify({
+              email,
+              password,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).then((res) => res.json() as Promise<{ data: { access_token: string } }>);
+          bearerToken = responseBody.data.access_token;
+        } else {
+          bearerToken = token;
+        }
+
+        const headers = {
+          Authorization: "Bearer " + bearerToken,
+        };
+
+        const collections = await fetch(path.join(host, "/collections"), { headers })
+          .then(
+            (res) => res.json() as Promise<{ data: DirectusCollection[] } | { errors: string[] }>
+          )
+          .then((res) => {
+            if ("errors" in res) {
+              throw new Error(
+                `One or more errors occured when fetching the collections: \n${res.errors.join("\n")}`
+              );
+            }
+            return res.data;
+          });
+
+        const fields = await fetch(path.join(host, "/fields"), { headers })
+          .then((res) => res.json() as Promise<{ data: DirectusField[] } | { errors: string[] }>)
+          .then((res) => {
+            if ("errors" in res) {
+              throw new Error(
+                `One or more errors occured when fetching the fields: \n${res.errors.join("\n")}`
+              );
+            }
+            return res.data;
+          });
+
+        const relations = await fetch(path.join(host, "/relations"), { headers })
+          .then((res) => res.json() as Promise<{ data: DirectusRelation[] } | { errors: string[] }>)
+          .then((res) => {
+            if ("errors" in res) {
+              throw new Error(
+                `One or more errors occured when fetching the relations: \n${res.errors.join("\n")}`
+              );
+            }
+            return res.data;
+          });
+
+        const types = generateTypes(
+          { collections, fields, relations },
+          {
+            typePrefix: "",
+            requiredNotNullable: false,
+          }
+        );
+
+        if (output == "") {
+          process.stdout.write(types + "\n");
+          return;
+        }
+
+        const resolvedPath = path.isAbsolute(output) ? output : path.resolve(process.cwd(), output);
+
+        if (fs.existsSync(resolvedPath)) {
+          process.stdout.write("This file already exists and will be overwritten.\n");
+          const confirmed = await confirm({
+            message: "Are you sure?",
+            default: false,
+          });
+          if (!confirmed) return;
+        }
+
+        await fsp.writeFile(resolvedPath, types);
+      } catch (err) {
+        if (err instanceof Error && err.name === "ExitPromptError") {
+          console.log("Aborting... 👋 Until next time!");
+        } else {
+          throw err;
+        }
       }
-
-      const resolvedPath = path.isAbsolute(output) ? output : path.resolve(process.cwd(), output);
-
-      if (fs.existsSync(resolvedPath)) {
-        process.stdout.write("This file already exists and will be overwritten.\n");
-        const confirmed = await confirm({
-          message: "Are you sure?",
-          default: false,
-        });
-        if (!confirmed) return;
-      }
-
-      await fsp.writeFile(resolvedPath, types);
     }
   )
   .help()
